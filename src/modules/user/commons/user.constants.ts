825c8654fc28503034444f568f300a9a8f3b7ee2
waqarRns <waqar@rnssol.com>
2022-04-29 10:31:58 +0500

    Bot exchange field was added to env file

--- src/modules/user/commons/user.constants.ts
+++ src/modules/user/commons/user.constants.ts
@@ -2,7 +2,6 @@ export const EarningLimit = 5;
 export const MaxLevels = 12;
 export const MaxProfitLimit = 90;
 export const botConstants = {
-  exchange: 'BINANCE',
   strategy: 'BUYSELL',
   riskLevel: 'LOW',
 };
